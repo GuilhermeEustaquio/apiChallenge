b3f49af664ebc1b9419f65f592974046090798da
Guilherme Eustaquio <143026960+GuilhermeEustaquio@users.noreply.github.com>
2026-05-19 10:10:51 -0300

    fix: correct ConexaoFactory to use getOrDefault with actual credentials as fallback

--- src/main/java/org/acme/conexao/ConexaoFactory.java
+++ src/main/java/org/acme/conexao/ConexaoFactory.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConexaoFactory {
 
-    private static final String URL = System.getenv("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL");
-    private static final String USER = System.getenv("rm566784");
-    private static final String PASSWORD = System.getenv("250305");
+    private static final String URL = System.getenv().getOrDefault("DB_URL", "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL");
+    private static final String USER = System.getenv().getOrDefault("DB_USER", "rm566784");
+    private static final String PASSWORD = System.getenv().getOrDefault("DB_PASSWORD", "250305");
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("oracle.jdbc.OracleDriver");
